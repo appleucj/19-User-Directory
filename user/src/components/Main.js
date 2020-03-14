9f5adc8742f4a88dac2b931030e4a1f47e204a4f
appleucj <58881608+appleucj@users.noreply.github.com>
2020-03-14 16:41:04 -0400

    update

---
+++ user/src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DataArea from "./DataArea";
+import"../styles/Main.css";
+function Main(){
+    return(
+        <>
+        <DataArea />
+        </>
+
+    );
+};
+export default Main;
